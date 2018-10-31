c071874f622e3ee73a0f0cb5a09bbf17a285f59c
Kim Hammar <kimham@kth.se>
2018-10-31 18:14:57 +0100

    add feature_engineering jobs template

---
+++ feature_engineering_spark/src/main/scala/limmen/github/com/feature_engineering_spark/featuregroups/AlertFeatures.scala
@@ -0,0 +1,18 @@
+package limmen.github.com.feature_engineering_spark.featuregroup
+
+/**
+  * Contains logic for computing the alert_features featuregroup
+  */
+object AlertFeatures {
+
+  /**
+    * Computes the featuregroup
+    *
+    * @param input path to the input dataset to read (csv)
+    * @param output name of the output featuregroup table
+    * @param version version of the featuregroup
+    * @param partitions number of spark partitions to parallelize the compute on
+    */
+  def computeFeatures(input: String, featuregroupName: String, version: Int, partitions: Int): Unit = {
+  }
+}
